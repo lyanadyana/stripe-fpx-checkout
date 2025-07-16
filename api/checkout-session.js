cc55d6451ef6793bd292d52c9c8304973c568ac5
lyanadyana <lyanadyanamy@gmail.com>
2025-07-16 18:00:01 +0800

    Add files via upload

---
+++ api/checkout-session.js
@@ -0,0 +1,34 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).send({ message: 'Method not allowed' });
+  }
+
+  const { amount, method, product } = req.body;
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: [method],
+      line_items: [
+        {
+          price_data: {
+            currency: 'myr',
+            product_data: {
+              name: product,
+            },
+            unit_amount: amount,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'https://lyanadyana.com/success',
+      cancel_url: 'https://lyanadyana.com/cancel',
+    });
+
+    res.status(200).json({ url: session.url });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
