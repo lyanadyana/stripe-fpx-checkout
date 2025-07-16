6c6bafcfde28ea17b5f726755206e529b8f9face
lyanadyana <lyanadyanamy@gmail.com>
2025-07-16 22:13:36 +0800

    Update checkout-session.js

--- api/checkout-session.js
+++ api/checkout-session.js
@@ -7,9 +7,9 @@ export default async function handler(req, res) {
     return res.status(405).json({ message: 'Method not allowed' });
   }
 
-  const { amount, method, product, name, phone, address } = req.body;
+  const { amount, method, product, customer } = req.body;
 
-  if (!amount || !method || !name || !phone || !address) {
+  if (!amount || !method || !customer || !customer.name || !customer.address) {
     return res.status(400).json({ message: 'Missing required fields' });
   }
 
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
           price_data: {
             currency: 'myr', // Mata wang yang digunakan
             product_data: {
-              name: product || "Product", // Nama produk
+              name: product, // Nama produk
             },
             unit_amount: amount, // Jumlah bayaran dalam sen
           },
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
         },
       ],
       mode: 'payment', // Mod pembayaran
-      success_url: `https://www.lyanadyana.com/success?name=${name}&phone=${phone}&address=${address}`, // URL kejayaan dengan parameter
+      success_url: `https://www.lyanadyana.com/success?name=${customer.name}&address=${customer.address}`, // URL kejayaan dengan parameter
       cancel_url: 'https://www.lyanadyana.com/cancel', // URL pembatalan
     });
 
